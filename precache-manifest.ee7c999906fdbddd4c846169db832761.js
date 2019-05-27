f74a07142ec653be8e3901b84de569be579cd4d9
dimple23 <dimplechavda23@gmail.com>
2019-05-27 18:00:25 -0400

    Updates

---
+++ precache-manifest.ee7c999906fdbddd4c846169db832761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b08b1ce420924b67594a0c6d74ed2a5",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "67be328064bafb77edbc",
+    "url": "/clicky-game/static/css/main.b4dd26a3.chunk.css"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/clicky-game/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "67be328064bafb77edbc",
+    "url": "/clicky-game/static/js/main.48739ec3.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "1729cf4050a8669c39380aaf830eb199",
+    "url": "/clicky-game/static/media/clicky.1729cf40.jpg"
+  }
+]);
